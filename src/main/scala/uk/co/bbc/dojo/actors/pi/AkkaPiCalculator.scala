921e2dd2607fa99606bea2047ae6cf2a4ebb99f3
Russell Broughton <russell.broughton@bbc.co.uk>
2016-01-23 17:34:13 +0000

    Added more explanatory comments to the Akka Pi example

--- src/main/scala/uk/co/bbc/dojo/actors/pi/AkkaPiCalculator.scala
+++ src/main/scala/uk/co/bbc/dojo/actors/pi/AkkaPiCalculator.scala
@@ -11,20 +11,20 @@ import scala.concurrent.Await
 
 /**
  * Based (a bit) on code from: http://doc.akka.io/docs/akka/2.0/intro/getting-started-first-scala.html
- * TODO: Add more comments.
  */
 class AkkaPiCalculator(numberOfIterations: Long) extends PiCalculator {
   private val numberOfWorkerActors = 8
   private val iterationsPerActorMessage = 1000000l
 
   override def calculatePi: Double = {
-    //Create the Akka system.
+    // Create the Akka system. There should be only one. Akka component names should not feature spaces or non-alphanumeric characters.
     val system = ActorSystem("Pi-System")
 
-    // The system creates a top-level actor automatically that all user created actors extend from. We now create our master actor that will be a child of it.
+    // Create our coordinating master to be a top-level actor - i.e. one that sits directly below the Akka System (technically, it sits below the top-level guardian. See here for more info http://doc.akka.io/docs/akka/2.4.1/general/supervision.html)
     val master = system.actorOf(Props(new Master(numberOfWorkerActors, iterationsPerActorMessage, numberOfIterations)), name = "master")
 
-    // Start the calculation. We could use the exclamation mark, 'fire and forget' (don't block) message notation, but instead we want a response, so we use the ?
+    // Message the master actor to start the calculation. '?' is override to send a message that we expect a response from (the Akka 'Ask' pattern).
+    // To send a non-blocking 'fire and forget' message, which is the norm, use '!' e.g. '<actor> ! <message>
     implicit val timeout = Timeout(Duration.create(1, MINUTES))
     val futureResponse = master ? Calculate
 
@@ -34,45 +34,58 @@ class AkkaPiCalculator(numberOfIterations: Long) extends PiCalculator {
   }
 }
 
-  class Master(numberOfWorkerActors: Int, iterationsPerActorMessage: Long, totalNumberOfIterations: Long) extends Actor {
-    private var actorToSendResultTo: ActorRef = _ //TODO: Try and do this a slicker way.
-    private var currentPi: Double = 0
-    private var numberOfResultsRecieved: Int = 0
+/**
+ * The master is the coordinating actor and is a freestanding, autonomous actor that communicates purely in terms of messages.
+ */
+class Master(numberOfWorkerActors: Int, iterationsPerActorMessage: Long, totalNumberOfIterations: Long) extends Actor {
+  private var actorToSendResultTo: ActorRef = _
+  private var currentPi: Double = 0
+  private var numberOfResultsRecieved: Int = 0
 
-    private val workerRouter = context.actorOf(Props[Worker].withRouter(RoundRobinPool(numberOfWorkerActors)), name = "workerRouter")
+  def receive = {
+    case Calculate =>
+      // Keep tract of the actor that we will be sending the result to (a temporary one created to fulfill the ask pattern) TODO: Add link for further detail.
+      actorToSendResultTo = sender
 
-    def receive = {
-      case Calculate =>
-        actorToSendResultTo = sender
-        for (i <- 0l until totalNumberOfMessages) workerRouter ! Work(i * iterationsPerActorMessage, iterationsPerActorMessage)
-      case Result(value) =>
-        currentPi += value
-        numberOfResultsRecieved += 1
-        if (numberOfResultsRecieved == totalNumberOfMessages) {
-          // Send the result to the listener
-          actorToSendResultTo ! PiApproximation(currentPi * 4.0)
-          // Stops this actor and all its supervised children
-          context.stop(self)
-        }
-    }
+      // We create a worker router. This will spawn worker actors and dispatch messages to them in turn using a round robin strategy.
+      // Note that each actor has a mailbox used to buffer messages received as it processes the current message.
+      val workerRouter = context.actorOf(Props[Worker].withRouter(RoundRobinPool(numberOfWorkerActors)), name = "workerRouter")
+      for (i <- 0l until totalNumberOfMessages) workerRouter ! Work(i * iterationsPerActorMessage, iterationsPerActorMessage)
+    case Result(value) =>
+      //When we receive a result back from a worker, we update our accumulators.
+      currentPi += value
+      numberOfResultsRecieved += 1
 
-    private def totalNumberOfMessages: Long = {
-      (totalNumberOfIterations % iterationsPerActorMessage) match {
-        case 0 => totalNumberOfIterations / iterationsPerActorMessage
-        case _ => totalNumberOfIterations / iterationsPerActorMessage + 1
+      if (numberOfResultsRecieved == totalNumberOfMessages) {
+        // When all of the results are in, then message whoever kicked off the calculation with the final result.
+        actorToSendResultTo ! PiApproximation(currentPi * 4.0)
+
+        // Stops this actor and all its supervised children. Will happen anyway when the Akka system stops, so not really needed, but good practice.
+        context.stop(self)
       }
-    }
   }
 
-  sealed trait PiMessage
-  case object Calculate extends PiMessage
-  case class Work(start: Long, nrOfElements: Long) extends PiMessage
-  case class Result(value: Double) extends PiMessage
-  case class PiApproximation(pi: Double)
-
-  class Worker extends Actor{
-    override def receive = {
-      case Work(start, nrOfElements) =>
-        sender ! Result(LiebnizPi.evaluateLiebniz(start, nrOfElements)) // perform the work
+  private def totalNumberOfMessages: Long = {
+    (totalNumberOfIterations % iterationsPerActorMessage) match {
+      case 0 => totalNumberOfIterations / iterationsPerActorMessage
+      case _ => totalNumberOfIterations / iterationsPerActorMessage + 1
     }
   }
+}
+
+// The worker class. Each one will calculate Pi for a specific range of the 'niz
+class Worker extends Actor{
+  override def receive = {
+    case Work(start, nrOfElements) =>
+      //Each actor has a context which provides lots of useful info / functionality. Prior to the receive method being called the sender is set in it.
+      context.sender ! Result(LiebnizPi.evaluateLiebniz(start, nrOfElements))
+  }
+}
+
+// All of the messages we'll use. Normally, these would be defined with the actor classes that handle them.
+// Note that not making message immutable is *really* bad practice and shouldn't be done.
+sealed trait PiMessage
+case object Calculate extends PiMessage
+case class Work(start: Long, nrOfElements: Long) extends PiMessage
+case class Result(value: Double) extends PiMessage
+case class PiApproximation(pi: Double)
