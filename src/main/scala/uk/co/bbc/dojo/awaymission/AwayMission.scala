570fb67f2dffc03e33b31d5bc382aca3d90db85a
Russell Broughton <russell.broughton@bbc.co.uk>
2016-01-28 10:40:01 +0000

    Removed solution code

--- src/main/scala/uk/co/bbc/dojo/awaymission/AwayMission.scala
+++ src/main/scala/uk/co/bbc/dojo/awaymission/AwayMission.scala
@@ -1,12 +1,6 @@
 package uk.co.bbc.dojo.awaymission
 
-import akka.pattern.ask
-import akka.actor.ActorSystem
 import akka.util.Timeout
-import akka.util.Timeout
-import com.typesafe.config.ConfigFactory
-import uk.co.bbc.dojo.awaymission.actors.StarshipCommand
-import uk.co.bbc.dojo.awaymission.actors.StarshipCommand.SeekOutNewLifeAndNewCivilisations
 import uk.co.bbc.dojo.awaymission.locations.Planet
 
 import scala.concurrent.Await
@@ -14,12 +8,8 @@ import scala.concurrent.duration._
 
 class AwayMission {
   def surveyPlanets(planetsToSurvey: List[Planet]): Int = {
-    val akkaSystem = ActorSystem("The-Corporation")
-
-    val starshipCommand = akkaSystem.actorOf(StarshipCommand(), name = "Admiral-Reith")
-
     implicit val timeout = Timeout(Duration.create(20, SECONDS))
-    val futureResponse = starshipCommand ? SeekOutNewLifeAndNewCivilisations(planetsToSurvey)
+    val futureResponse = ???
 
     val occupiedPlanets = Await.result(futureResponse, timeout.duration).asInstanceOf[Int]
     occupiedPlanets
